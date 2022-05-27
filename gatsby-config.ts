d345f0d2850ed3c25291b7efac57ef291528f4b0
Alexander Burkov <alexander.burkov@jetbrains.com>
2022-05-27 15:32:00 +0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `TSGD24K2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
